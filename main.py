45cc06b8224015fdac0eeeaaf9f10095c6b3e2db
viktor-akusoff <kirill.voloshin.98@ya.ru>
2023-11-11 19:07:23 +0300

    play control update

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from ui_main import Ui_MainWindow
 class Mode(Enum):
     DRAWING = 1
     ERASING = 2
+    PLAYING = 3
 
 
 class LifeOdyssey(QMainWindow):
@@ -25,6 +26,7 @@ class LifeOdyssey(QMainWindow):
         self.timer.timeout.connect(self.next_iteration)
         self.is_playing = False
         self.mode = Mode.DRAWING
+        self.prev_mode = None
 
         stopButton = self.ui.stopButton
         stopButton.setCheckable(True)
@@ -58,60 +60,90 @@ class LifeOdyssey(QMainWindow):
         frameSpinBox.valueChanged.connect(self.update_value)
 
     def stop_button(self):
-        self.iteration_number = 0
-        self.update_frame()
+        self.stop_playing()
+        self.ui.frameSpinBox.setValue(0)
+        self.restore_mode()
+        self.update_mode_indicator()
+
+    def set_play_mode(self):
+        if self.mode != Mode.PLAYING:
+            self.switch_mode(Mode.PLAYING)
 
     def play_button(self):
+        self.switch_mode(Mode.PLAYING)
         if self.is_playing:
-            self.is_playing = False
-            self.timer.stop()
-            self.ui.playButton.setIcon(QIcon(u":/icons/icons/play_arrow.svg"))
+            self.stop_playing()
             return
-        self.ui.playButton.setIcon(QIcon(u":/icons/icons/pause.svg"))
+        self.start_playing()
+
+    def start_playing(self):
         self.is_playing = True
         self.timer.start(250)
+        self.ui.playButton.setIcon(QIcon(u":/icons/icons/pause.svg"))
+
+    def stop_playing(self):
+        self.is_playing = False
+        self.timer.stop()
+        self.ui.playButton.setIcon(QIcon(u":/icons/icons/play_arrow.svg"))
+
+    def move_spinbox(self, num):
+        old_val = self.ui.frameSpinBox.value()
+        self.ui.frameSpinBox.setValue(old_val + num)
+        return old_val
 
     def jump_forward_button(self):
-        self.iteration_number += 5
-        if self.iteration_number > 99:
-            self.iteration_number = 99
-        self.update_frame()
+        self.set_play_mode()
+        old_val = self.move_spinbox(5)
+        if old_val > 94:
+            self.ui.frameSpinBox.setValue(0)
 
     def jump_backward_button(self):
-        self.iteration_number -= 5
-        if self.iteration_number < 0:
-            self.iteration_number = 0
-        self.update_frame()
+        self.set_play_mode()
+        old_val = self.move_spinbox(-5)
+        if old_val < 5:
+            self.ui.frameSpinBox.setValue(99)
 
     def palette_button(self):
         print("PALETTE")
 
     def draw_button(self):
-        self.mode = Mode.DRAWING
-        self.update_mode()
+        self.stop_button()
+        self.switch_mode(Mode.DRAWING)
+        self.update_mode_indicator()
 
     def erase_button(self):
-        self.mode = Mode.ERASING
-        self.update_mode()
-
-    def update_frame(self):
-        self.ui.frameSpinBox.setValue(self.iteration_number)
+        self.stop_button()
+        self.switch_mode(Mode.ERASING)
+        self.update_mode_indicator()
 
     def update_value(self):
+        if self.mode != Mode.PLAYING:
+            self.switch_mode(Mode.PLAYING)
         self.iteration_number = self.ui.frameSpinBox.value()
 
     def next_iteration(self):
-        self.curr_time = self.curr_time.addSecs(0.25)
-        self.iteration_number += 1
-        if self.iteration_number > 99:
-            self.iteration_number = 0
-        self.update_frame()
+        self.curr_time = self.curr_time.addSecs(1)
+        old_val = self.move_spinbox(1)
+        if old_val == 99:
+            self.ui.frameSpinBox.setValue(0)
 
-    def update_mode(self):
+    def update_mode_indicator(self):
         if self.mode == Mode.DRAWING:
             self.ui.modeLabel.setText("Режим: рисование")
         elif self.mode == Mode.ERASING:
             self.ui.modeLabel.setText("Режим: стирание")
+        elif self.mode == Mode.PLAYING:
+            self.ui.modeLabel.setText("Режим: проигрывание")
+
+    def switch_mode(self, mode):
+        if self.mode != mode:
+            self.prev_mode = self.mode
+            self.mode = mode
+            self.update_mode_indicator()
+
+    def restore_mode(self):
+        if self.mode == Mode.PLAYING:
+            self.mode, self.prev_mode = self.prev_mode, self.mode
 
 
 if __name__ == "__main__":
